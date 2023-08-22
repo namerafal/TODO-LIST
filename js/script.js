e0270196763b829e8c4df3fbde83e15b100af4a8
Rafał Olewicz <namerafal75@gmail.com>
2023-08-22 21:49:36 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+{
+  const tasks = [];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+
+    render();
+
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+
+    render();
+
+  };
+
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButtons, index) => {
+      removeButtons.addEventListener("click", () => {
+        removeTask(index);
+
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+
+      });
+    });
+  };
+
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+              <li
+              class="list__item${task.done ? " list__item--done" : ""}"
+              >
+              <button class="js-done">zrobione?</button>
+              <button class="js-remove">🗑</button>
+                  ${task.content} 
+              </li>
+              `;
+    };
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
+  };
+
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskElement = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskElement.value.trim();
+
+    if (newTaskContent !== "") {
+        addNewTask(newTaskContent);
+        newTaskElement.value = "";
+    }
+
+    newTaskElement.focus();
+  };
+
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", onFormSubmit);
+
+  };
+
+  init();
+}
